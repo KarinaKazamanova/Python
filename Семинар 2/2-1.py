ec4ed04795503f3fd95e681d8b8ac782bb50bf2d
Karina <k5524392@gmail.com>
2022-09-27 17:33:33 +0300

    Решение задачи 1

--- Семинар 2/2-1.py
+++ Семинар 2/2-1.py
@@ -1,10 +1,10 @@
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 
 
-def sum_dig(a):
-    b = 0
-    while (a):
-        b += a%10
-        a = a//10
-    return b
+def sum_dig(number):
+    sum_of_digits = 0
+    while (number):
+        sum_of_digits += number%10
+        number = number//10
+    return sum_of_digits
 print(sum_dig(int(input("Введите число: "))))
