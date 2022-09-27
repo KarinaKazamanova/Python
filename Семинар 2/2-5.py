d54c0ea31b2536559f4936c066efc0c052734256
Karina <k5524392@gmail.com>
2022-09-27 16:34:50 +0300

    Решение задачи 5

---
+++ Семинар 2/2-5.py
@@ -0,0 +1,14 @@
+# Реализуйте алгоритм перемешивания списка. Без функции suffle из модуля random.
+
+import random
+my_massiv = []
+for i in range (int(input("Введите длину массива: "))):
+    my_massiv.append(i)
+print(my_massiv)
+for i in range (len(my_massiv)-1):
+    j = random.randint(i, (len(my_massiv)-1))
+    temp = 0
+    temp = my_massiv [i] 
+    my_massiv [i] = my_massiv[j]
+    my_massiv[j] = temp
+print(my_massiv)
