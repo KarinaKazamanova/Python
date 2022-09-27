d06779193ca538141a3b28b2cd744cb02b03018d
Karina <k5524392@gmail.com>
2022-09-27 16:54:54 +0300

    Решение задачи 5

--- Семинар 1/1-5.py
+++ Семинар 1/1-5.py
@@ -1,14 +1,26 @@
 # Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
 import math
+def client_input(a):
+    result = -1
+    while (result):
+        try:
+            result = float (input(a))
+        except:
+            
+            result = -1
+        finally:
+            return result
 
-c = -1
-while (c):
-    a = [float(i) for i in input("Введите координаты точки Х через запятую: ").split(',')]
-    b = [float(i) for i in input("Введите координаты точки Y через запятую: ").split(',')]
+a = [float(i) for i in input("Введите координаты точки Х через пробел: ").split()]
+b = [float(i) for i in input("Введите координаты точки Y через пробел: ").split()]
+while (a and b):
     if (len(a) == len (b) == 2):
-        
-        print(f"Расстояние между Х и У = {math.sqrt((math.pow((b[0] - a[0]),2) + math.pow((b[1] - a[1]),2)))//1 + math.sqrt((math.pow((b[0] - a[0]),2) + math.pow((b[1] - a[1]),2)))*100%100//1/100}")
-        c = 0
+        distance = math.sqrt((math.pow((b[0] - a[0]),2) + math.pow((b[1] - a[1]),2)))//1 + math.sqrt((math.pow((b[0] - a[0]),2) + math.pow((b[1] - a[1]),2)))*100%100//1/100
+        print(f"Расстояние между Х и У = {distance}")
+        a = 0
+        b = 0
     else:
         print ("Введите две координаты")
-        с = -1
+        a = [float(i) for i in input("Введите координаты точки Х через пробел: ").split()]
+        b = [float(i) for i in input("Введите координаты точки Y через пробел: ").split()]      
+       
