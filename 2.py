9c16bcb4117e934ee01b19581d1f28c19e2ec40f
Karina <k5524392@gmail.com>
2022-09-27 09:06:43 +0300

    Решение задачи 1

--- 2.py
+++
@@ -1,6 +0,0 @@
-# Напишите программу для проверки истинности утверждения ¬(X ⋁(или) Y ⋁ Z) = ¬X ⋀ (и) ¬Y ⋀ ¬Z для всех значений предикат.
-
-for a in range (2):
-    for b in range (2):
-        for c in range (2):
-            print(f"for X = {a}, Y = {b} and Z = {c} ¬(X ⋁(или) Y ⋁ Z) = ¬X ⋀ (и) ¬Y ⋀ ¬Z {not (a or b or c)==(not a and not b and not c)}")
