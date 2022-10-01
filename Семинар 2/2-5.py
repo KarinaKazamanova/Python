2ab43170bce29827af7a8f453f3f5f09cb07ef07
Karina <k5524392@gmail.com>
2022-10-01 12:33:51 +0300

    Решение задачи Т

--- Семинар 2/2-5.py
+++ Семинар 2/2-5.py
@@ -3,14 +3,11 @@
 import random
 my_massiv = []
 for i in range (int(input("Введите длину массива: "))):
-    my_massiv.append(i)
+    my_massiv.append(i*3)
 print(my_massiv)
 for i in range (len(my_massiv)-1):
-    j = random.randint(i, (len(my_massiv)-1))
-    temp = 0
-    temp = my_massiv [i] 
-    my_massiv [i] = my_massiv[j]
-    my_massiv[j] = temp
+    j = random.randint(i, (len(my_massiv)-1)) 
+    my_massiv [i], my_massiv[j] = my_massiv[j], my_massiv [i]
 print(my_massiv)
 
 # прочитала в учебнике "Алгоритмы. Теория и практическое применение"
