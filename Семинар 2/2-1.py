11005dea512c8b2adb03d508d6dcddc647ada093
Karina <k5524392@gmail.com>
2022-10-01 10:17:43 +0300

    Решение задачи 1

--- Семинар 2/2-1.py
+++ Семинар 2/2-1.py
@@ -3,8 +3,13 @@
 
 def sum_dig(number):
     sum_of_digits = 0
+    while number % 1 != 0:
+        number = number * 10
     while (number):
         sum_of_digits += number%10
         number = number//10
     return sum_of_digits
-print(sum_dig(int(input("Введите число: "))))
+
+a = abs(float((input("Введите число: "))))
+b = int(sum_dig(a))
+print(b)
