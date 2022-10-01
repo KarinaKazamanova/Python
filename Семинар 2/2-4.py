deb0a6b7a6d159dbc84135cfca3fb3bf39f25180
Karina <k5524392@gmail.com>
2022-10-01 11:15:39 +0300

    изменение позиций

--- Семинар 2/2-4.py
+++ Семинар 2/2-4.py
@@ -1,7 +1,7 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
 x = open('file.txt','r')
 position_1 = int(x.readline())
-position_2 = int(x.readline(1))
+position_2 = int(x.readline())
 n = int(input("Введите число: "))
 massive = []
 for i in range (-n,n+1):
