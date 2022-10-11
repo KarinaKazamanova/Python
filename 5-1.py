274703bb6d66e3a5f73b8fb4a081863f75ae9ad2
Karina <108223541+KarinaKazamanova@users.noreply.github.com>
2022-10-11 10:38:58 +0300

    Create 5-1.py

---
+++ 5-1.py
@@ -0,0 +1,17 @@
+a = input("Введите текст, который нужно врхивировать: ")  
+symbols = []
+s_count = []
+count = 1
+for i in range(1, len(a)):
+    if a[i - 1] == a[i]:
+        count += 1
+        continue
+    symbols.append(a[i-1])
+    s_count.append(count)
+    count = 1
+symbols.append(a[i])
+s_count.append(count)
+zip_a = list(map(lambda x, y: str(x) + str(y),s_count, symbols))
+print(f"Архивированная строка: {''.join(zip_a)}")
+unzip_a = list(map(lambda x, y: int(x) * str(y), s_count, symbols))
+print(f"Разархивированная строка: {''.join(unzip_a)}")
